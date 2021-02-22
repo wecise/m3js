a32e0697849dc6e8a305fdf3cbf655b9c8a2f524
cnwangzd <cnwangzd@gmail.com>
2021-02-22 10:31:54 +0800

    add job/dfs/consonlelog

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@
     // 测试环境
     if (process.env.NODE_ENV === "development") {
 
-      if (opts.url.indexOf("http://") === -1) {
+      if (opts.url.indexOf("http://") === -1 && opts.url.indexOf("https://") === -1) {
         if(opts.url.indexOf("?") === -1){
           _url = `${baseUrl}${opts.url}?sessionid=${sessionid}`;
         } else {
@@ -59,6 +59,7 @@
       type = opts.type || 'GET',
       url = _url,
       params = opts.data,
+      processData = opts.processData || true,
       dataType = opts.dataType || 'json';
 
     type = type.toUpperCase();
@@ -71,6 +72,7 @@
           str += prop + '=' + obj[prop] + '&'
         }
         str = str.slice(0, str.length - 1);
+        
         return str;
       })(opts.data);
       url += url.indexOf('?') === -1 ? '?' + params : '&' + params;
@@ -81,8 +83,16 @@
     if (opts.contentType) {
       xhr.setRequestHeader('Content-type', opts.contentType);
     }
-
-    xhr.send(params ? params : null);
+    
+    if (!opts.processData){
+      let fm = new FormData();
+      _.forEach(params,(v,k)=>{
+        fm.append(k,v);
+      })
+      xhr.send(params ? fm : null);
+    } else{
+      xhr.send(params ? params : null);
+    }
 
     //return promise
     return new Promise(function (resolve, reject) {
@@ -108,23 +118,31 @@
   /* 
   *  connect to M3 platform and return sessionid  For development env
   */
-  let connect = async function (url, port, company, username, password) {
+  let connect = async function (http, url, port, company, username, password) {
 
-      baseUrl = "http://" + [url, port].join(":");
+      baseUrl = http + "://" + [url, port].join(":");
 
       let opts = {
         url: `${baseUrl}/user/signin?company=${company}&username=${username}&password=${password}`,
         type: 'post'
       };
-      await ajax(opts).then((rtn) => {
-        sessionid = rtn.message;
-        
-        init();
 
-      }).catch((err) => {
-        console.log(err)
-        window.location.href=baseUrl;
-      });
+      return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          sessionid = rtn.message;
+          init();
+          resolve(rtn);
+        }).catch((err)=>{
+          window.location.href=baseUrl;
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
 
   };
 
@@ -133,14 +151,21 @@
    */
   let callFS = async function (fileName, input) {
     let opts = { url: `/script/exec/js?filepath=${fileName}&input=${input}&isfile=true`, type: 'POST' };
-    let rt = null;
-    await ajax(opts).then((rtn) => {
-      rt = rtn;
-    }).catch((err) => {
-      rt = err;
-    });
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
 
-    return rt;
   };
 
   /* 
@@ -176,39 +201,37 @@
   };
 
   /*
-  *
+  * 通过选项创建 VueI18n 实例
   */
-  // 通过选项创建 VueI18n 实例
   let lang = async function() {
     
-    try {
-
-      let cache = localStorage.getItem("M3-LANG-LIST");
-      
-      if (cache) {
+    return new Promise( await function (resolve, reject) {
         
-        // return new VueI18n({
-        //   locale: window.M3_LANG,
-        //   messages: JSON.parse(cache)
-        // });
+        let cache = localStorage.getItem("M3-LANG-LIST");
+        
+        if (cache) {
+          
+          resolve(JSON.parse(cache));
 
-        return JSON.parse(cache);
+        } else {
 
-      } else {
+          callFS("/matrix/lang/getLangList.js", null).then((rtn) => {
 
-        await callFS("/matrix/lang/getLangList.js", null).then((rtn) => {
+            localStorage.setItem("M3-LANG-LIST", JSON.stringify(rtn.message));
 
-          localStorage.setItem("M3-LANG-LIST", JSON.stringify(rtn.message));
+            resolve(rtn.message);
 
-          return rtn.message;
+          }).catch((err)=>{
+            if(typeof err === 'string'){
+              reject(JSON.parse(err));
+            } else {
+              reject(err);
+            }
+          });
 
-        });
-
-      }
-
-    } catch (err) {
-      return null;
-    }
+        }  
+        
+    })
 
   };
 
@@ -330,41 +353,279 @@ let setAppAsHomeForAllUser = async function(vm,item){
           document.title = auth.Company.title;
       }
   };
+  
+  /* 
+   * RULE 
+  */
+  let ruleGet = async function(data){
+    
+    let opts = { 
+        url: '/config/get', 
+        type: 'GET', 
+        data:{key:data}
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
+  };
+
+  let ruleAdd = function(){
+
+  };
+
+  let ruleDelete = function(){
+
+  };
+
+  let ruleExport = function(){
+
+  };
+
+  /* 
+   * JOB 
+    */
+  let jobNew = function(){
+
+  };
+
 
   /* 
    *  DFS 
   */
-  var dfsWrite = function () {
+  let dfsWrite = function () {
 
   };
 
-  var dfsRead = function () {
+  let dfsRead = function () {
 
   };
 
-  /* 全屏控制 */
-    let fullScreen = function(mode) {
-      if ( mode ) {
-          if (document.documentElement.requestFullscreen) {
-              document.documentElement.requestFullscreen();
-          } else if (document.documentElement.msRequestFullscreen) {
-              document.documentElement.msRequestFullscreen();
-          } else if (document.documentElement.mozRequestFullScreen) {
-              document.documentElement.mozRequestFullScreen();
-          } else if (document.documentElement.webkitRequestFullscreen) {
-              document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  let dfsNew = async function(data) {
+    
+    let opts = { 
+        url: `/fs${data.parent}/${data.name}${window.auth.isAdmin?'?issys=true':''}`, 
+        type: 'PUT', 
+        contentType: false,
+        processData: false,
+        data:data.data
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
           }
-      } else {
-          if (document.exitFullscreen) {
-              document.exitFullscreen();
-          } else if (document.msExitFullscreen) {
-              document.msExitFullscreen();
-          } else if (document.mozCancelFullScreen) {
-              document.mozCancelFullScreen();
-          } else if (document.webkitExitFullscreen) {
-              document.webkitExitFullscreen();
+          
+        })
+    })
+
+  };
+
+  let dfsDelete = async function(data) {
+    
+    let opts = { 
+        url: `/fs${data.parent}/${data.name}${window.auth.isAdmin?'?issys=true':''}`, 
+        type: 'DELETE', 
+        contentType: false
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
           }
+          
+        })
+    })
+
+  };
+
+  let dfsRename = async function(data){
+    
+    let opts = { 
+        url: `/fs/rename${window.auth.isAdmin?'?issys=true':''}`, 
+        type: 'POST', 
+        processData: false,
+        data
+    };
+
+    return new Promise( await function (resolve, reject) {
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
+
+  }
+  
+  let dfsUpdateAttr = async function(data){
+    let opts = { 
+        url: `/fs${data.parent}/${data.name}?type=attr${window.auth.isAdmin?'&issys=true':''}`, 
+        type: 'PUT', 
+        processData: false,
+        contentType: false,
+        data: {attr: data.attr}
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
+  }
+
+  /* Console Log */
+  let consolelogTrace = async function(data){
+      
+      let opts = { 
+          url: `/consolelog/${data.type}?name=${encodeURIComponent( data.name )}&limit=${data.param.limit}`, 
+          type: 'GET'
+      };
+
+      if(data.param.level != null && data.param.level.length > 0){
+          opts.url = `${opts.url}&level=${data.param.level.join("&level=")}`;
       }
+      
+      return new Promise( await function (resolve, reject) {
+          
+          ajax(opts).then((rtn) => {
+            resolve(rtn);
+          }).catch((err)=>{
+            if(typeof err === 'string'){
+              reject(JSON.parse(err));
+            } else {
+              reject(err);
+            }
+            
+          })
+      })
+  };
+
+  let consolelogDelete = async function(data){
+    let opts = { 
+        url: `/consolelog/${data.type}?name=${encodeURIComponent( data.name )}`, 
+        type: 'DELETE'
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
+  };
+
+  let consolelogTruncate = async function(data){
+    let opts = { 
+        url: `/consolelog/${data.type}/truncate`, 
+        type: 'DELETE'
+    };
+    
+    return new Promise( await function (resolve, reject) {
+        
+        ajax(opts).then((rtn) => {
+          resolve(rtn);
+        }).catch((err)=>{
+          if(typeof err === 'string'){
+            reject(JSON.parse(err));
+          } else {
+            reject(err);
+          }
+          
+        })
+    })
+  };
+
+
+  /* 告警视图结构 */
+  let EventViewDataObj = {
+    info:{
+      name: "新建视图",
+      desc: ""  
+    },
+    datasource: {
+      name: "数据源",
+      datasource: ""
+    },
+    view: {
+      columns: []
+    }
+  };
+
+  /* Utils */
+  // 单位转换
+  let bytesToSize = function(bytes) {
+      var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+      if (bytes == 0) return '0 Byte';
+      var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+      return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+  };
+
+  // 全屏控制
+  let fullScreen = function(mode) {
+    if ( mode ) {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
   };
 
 
@@ -378,6 +639,26 @@ let setAppAsHomeForAllUser = async function(vm,item){
   exports.setAppAsHome = setAppAsHome;
   exports.setAppAsHomeForAllUser = setAppAsHomeForAllUser;
   exports.fullScreen = fullScreen;
+  /* dfs */
+  exports.dfsNew = dfsNew;
+  exports.dfsDelete = dfsDelete;
+  exports.dfsRename = dfsRename;
+  exports.dfsUpdateAttr = dfsUpdateAttr;
+  /* rule */
+  exports.ruleGet = ruleGet;
+  exports.ruleAdd = ruleAdd;
+  exports.ruleDelete = ruleDelete;
+  exports.ruleExport = ruleExport;
+  
+  /* console log */
+  exports.consolelogTrace = consolelogTrace;
+  exports.consolelogDelete = consolelogDelete;
+  exports.consolelogTruncate = consolelogTruncate;
+
+  exports.EventViewDataObj = EventViewDataObj;
+
+  /* utils */
+  exports.bytesToSize = bytesToSize;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
