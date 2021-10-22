716015d7ff1458bb2a21b3e58102a69aad801671
libf <libf@wecise.com>
2021-10-22 11:02:38 +0800

    m3js

--- m3app_template/vue.config.js
+++ m3app_template/vue.config.js
@@ -1,5 +1,5 @@
 // 从m3js模版加载vue cli配置信息
-m3config = require("@wecise/m3js/vue.config")
+const m3config = require("@wecise/m3js/vue.config")
 // 调整覆盖vue cli配置信息
 m3config.devServer.port = 8080
 // 输出vue cli配置信息
