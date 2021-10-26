b2c42d9645cb0cc9a65a11094519662659bad93b
libf <libf@wecise.com>
2021-10-26 22:32:47 +0800

    update npm

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 
-npm publish --access=public --registry=https://registry.npmjs.org/
-
 export over=`grep '"version":\s*".*"' package.json | awk -F '"' '{print $4}'`
 export nver=`echo "${over}" | awk -F "." '{print $1"."$2"."$3+1}'`
 #echo sed -e 's/"version":[ ]*"'${over}'"/"version": "'${nver}'"/' -i "" package.json
 sed -e 's/"version":[ ]*"'${over}'"/"version": "'${nver}'"/' -i "" package.json
 #cat package.json
+
+npm publish --access=public --registry=https://registry.npmjs.org/
