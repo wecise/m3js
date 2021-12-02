ffa12eac5d90c5238f637b26761c956b32eec4e9
cnwangzd@gmail.com <cnwangzd@gmail.com>
2021-12-02 14:59:39 +0800

    add rule export&import

--- src/rule/index.js
+++ src/rule/index.js
@@ -90,6 +90,38 @@ export let remove = function(data){
     
   };
 
-export let exportRule = function(){
-
-};
+  export let exportRule =  function(key,FileSaver){
+    return new Promise( function (resolve, reject) {
+      
+      http.get({
+        url: `/config/export?key=${key}`
+      }).then(res=>{
+        let blob = new Blob([JSON.stringify(res.data,null,2)], {type: "text/plain;charset=utf-8"});
+        let fileName = `ETCD${key}_${new Date().toLocaleString()}.json`;
+        FileSaver.saveAs(blob, fileName);
+        resolve(fileName);
+      }).catch(err=>{
+        reject(err);
+      })
+    })
+    
+  };
+  
+  export let importRule = function(file){
+  
+    return new Promise( function (resolve, reject) {
+      
+      let fm = new FormData();
+      fm.append("uploadfile", file);
+  
+      http.post({
+        url: `/config/import`,
+        param: fm,
+        headers: {'Content-Type':'multipart/form-data'}
+      }).then(res=>{
+        resolve(res.data);
+      }).catch(err=>{
+        reject(err.data);
+      })
+    })
+  };
